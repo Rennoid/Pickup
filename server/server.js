b5903b7ff59254aabc620a499f45014d9cb03102
Steven Shyun <stevenshuhyow@yahoo.com>
2015-05-04 16:23:58 -0700

    Added queries to edit DB

--- server/server.js
+++ server/server.js
@@ -1,11 +1,22 @@
 var express = require('express');
-var sequelize = require('sequelize');
+var parser = require('body-parser');
 
 //create express server listening on PORT 3000
 var app = express();
 
+//direct all route requests to router.js
+var router = require('./routes.js')
+
+//runs the server on port 3000
 var server = app.listen(3000, function (){
   var host = server.address().address;
   var port = server.address().port;
   console.log('Example app listening at http://%s:%s', host, port);
 });
+
+//ensures that all responses be stored in the body
+app.use(parser.json());
+
+
+// // Serve the client files
+// app.use(express.static(__dirname + "/../client"));
