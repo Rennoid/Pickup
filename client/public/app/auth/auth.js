9eb8b37065e13fd641348bac0ff1f98551453a71
Mark Bennett <mwbennett@wesleyan.edu>
2015-05-06 23:52:57 -0400

    (feat) created basic structure for user auth - still needs tweaking

--- client/public/app/auth/auth.js
+++ client/public/app/auth/auth.js
@@ -1,14 +1,28 @@
 angular.module('app.auth', [])
 
 // create AuthController to handle login and signup functions 
-.controller('AuthController', function ($scope, $location) {
+.controller('AuthController', function ($scope, $location, Auth) {
   $scope.user = {};
 
   $scope.login = function () {
-    // In shortly this called to an Auth service. Should we do the same? 
+   Auth.login($scope.user)
+     .then(function (token) {
+       $window.localStorage.setItem('com.app', token);
+       $location.path('/');
+     })
+     .catch(function (error) {
+       console.error(error);
+     });
   };
 
   $scope.signup = function () {
-    // In shortly this called to an Auth service. Should we do the same? 
+    Auth.signup($scope.user)
+      .then(function (token) {
+        $window.localStorage.setItem('com.app', token);
+        $location.path('/');
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
   };
 });
