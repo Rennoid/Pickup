dc4e1def4638f495a7d1c7d2eb1ae68929ffe12f
Mark Bennett <mwbennett@wesleyan.edu>
2015-05-10 21:59:05 -0700

    [feat] implemented funcationality for user rsvp submission

--- client/public/app/courts/courts.js
+++ client/public/app/courts/courts.js
@@ -1,6 +1,23 @@
 angular.module('app.courts', [])
 
 // create CourtController to send data from database to the view 
-.controller('CourtController', function ($scope, Court) {
+.controller('CourtController', function ($scope, $window, Court) {
   $scope.court = Court;
+  $scope.rsvp = {};
+  
+  $scope.addRsvp = function () {
+    var date = $scope.rsvp.date;
+    var starttime = $scope.rsvp.starttime;
+    var endtime = $scope.rsvp.endtime;
+    var courtName = $scope.court.currentCourtData.name;
+    var user = JSON.parse($window.localStorage.getItem('com.app'));
+    var rsvp = {
+      'date': date,
+      'starttime': starttime,
+      'endtime': endtime,
+      'courtName': courtName,
+      'userId': user.userId
+    };
+    Court.postRsvp(rsvp);
+  };
 });
