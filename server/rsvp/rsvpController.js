506b47815f6a6c0f2edb8ce712ab3c9b7597c4ef
Mark Bennett <mwbennett@wesleyan.edu>
2015-05-11 12:21:28 -0700

    deleted unnecessary comments

--- server/rsvp/rsvpController.js
+++ server/rsvp/rsvpController.js
@@ -1,39 +1,5 @@
 var db = require('../db');
 
-// var addCourt = function(req, res, next){
-//   db.Court.create({
-//     name: req.body.courtName,
-//     address:req.body.address,
-//     // longitude: req.body.longitude,
-//     // lattitude:req.body.lattitude,
-//     // rating: req.body.rating
-//   }).then(function(results){
-//     return results;
-//   });
-// };
-
-// var findCourt = function(req, res, next){
-//   var courtName = req.body.courtName;
-//   console.log("LINE 17 COURTNAME: ", courtName);
-//   db.Court.find({where: {courtname: courtName }})
-//     .then(function(court){
-//       console.log("LINE 20 COURT: ", court);
-//       if(!court) {
-//         addCourt(req, res, next)
-//           .then(function(result) {
-//             console.log('LINE 24 RESULT: ', result);
-//             var courtID = result.id;
-//             next(courtID);
-//           });
-//       }
-//       var courtID = db.Court.court.id;
-//       next(courtID);
-//       })
-//     .catch(function(error){
-//       next(new Error('Unable to find court ID: ', error));
-//     });
-// };
-
 var addRsvp = function (req, res, next){
   var starttime = req.body.starttime,
     endtime = req.body.endtime,
@@ -70,7 +36,6 @@ var findRsvp = function(req, res, next){
 
   db.RSVP.findAll({where:{UserId: userId}, include:[db.Court]})
     .then(function (results){
-      console.log("RESULTS: ", results);
       res.json(results);
     })
     .catch(function(error){
@@ -93,9 +58,7 @@ var allRsvp = function(req, res, next){
 };
 
 module.exports = {
-  // findCourt: findCourt,
   addRsvp: addRsvp,
   findRsvp: findRsvp,
   allRsvp: allRsvp
-  // addCourt: addCourt
 };
